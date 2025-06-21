fbfd97993a417563339b608e9e377936ad07936b
laurenannmurugi <laurenann1871@gmail.com>
2025-06-21 16:22:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+
+// create an eventlistener
+
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraph = document.querySelector('p');
+  paragraph.textContent = "This is really cool!";
+});
+
+
+
+
+
